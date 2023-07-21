ce5f5b66b060900b2dbf5e5a2dd5e78f7a2b643b
Francisco Sacroisky <sacroisky.f@gmail.com>
2023-07-21 17:27:14 +0200

    first components

--- projects/tic-tac-toe/src/App.jsx
+++ projects/tic-tac-toe/src/App.jsx
@@ -1,63 +1,54 @@
 import { useState } from "react";
+import confetti from "canvas-confetti";
 import "./App.css";
-
-const TURNS = {
-  X: "x",
-  O: "o",
-};
-
-// children = va a mostrar si queremos una x o una "o"
-//updateBoard forma de actualizar el tablero.
-
-//el Square es cada posicion ("cuadradito")
-const Square = ({ children, updateBoard, isSelected, index }) => {
-  const className = `square ${isSelected ? 'is-selected' : ''}`;
-
-  const handleClick = () => {
-    updateBoard(index)
-  };
-  //Se aplica renderizado condicional al que sea que le toque el turno
-  return (
-    <div onClick={handleClick} className={className}>
-      {children}
-    </div>
-  );
-};
+import { Square } from "./components/Square.jsx";
+import { TURNS } from "./constants.js";
+import { checkWinnerFrom, checkEndGame } from "./logic/board.js";
+import { WinnerModal } from "./components/WinnerModal";
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [turn, setTurn] = useState(TURNS.X);
-  //Detectar cuando tenemos un ganador: para hacer click y actualizar el tablero
-  //necesitamos otro estado para saber de quien es el turno..si de "x" o de "o"
+  const [winner, setWinner] = useState(null);
+  // null si no hay ganador
+  //Detectar cuando tenemos un ganador: para hacer click yactualizar el tablero
+  //necesitamos otro estado para saber de quien es el turno.si de "x" o de "o"
+  const resetGame=()=>{
+    setBoard(Array(9).fill(null));
+    setTurn(TURNS.X);
+    setWinner(null);
+}   
 
-const updateBoard=(index)=>{
-  //no actualizamos esta posicion
-  //si ya tiene algo
-  if(board[index])return
-  const newBoard=[... board]
-  newBoard[index]=turn
-  setBoard(newBoard)
-  //para que la linea se actualice y se vea visualmente
- const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
- setTurn(newTurn)
- 
-
-}
-
+  const updateBoard = (index) => {
+    //no actualizamos esta posicion
+    //si ya tiene algo
+    if (board[index] || winner) return;
+    //actualizar el tablero
+    const newBoard = [...board]; //24min del video
+    newBoard[index] = turn;
+    setBoard(newBoard);
+    // cambiar el turno
+    //para que la linea se actualice y se vea visualmente 
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn);
+    const newWinner = checkWinnerFrom(newBoard);
+    if (newWinner) {
+      confetti();
+      setWinner(newWinner);
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false); //empate
+    }
+  };
 
   return (
     <main className="board">
-      <h1>Ta Te Ti</h1>
+      <h1>Tic Tac Toe</h1>
+      <button onClick={resetGame}>Reset</button>
       <section className="game">
-        {
-        board.map((_, index) => {
+        {board.map((square, index) => {
           return (
-            <Square 
-            key={index}
-            index={index} 
-            updateBoard={updateBoard}
-            >
-              {board[index]}
+            <Square key={index} index={index} updateBoard={updateBoard}>
+              {square}
             </Square>
           );
         })}
@@ -66,11 +57,11 @@ const updateBoard=(index)=>{
         <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
         <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
+   <WinnerModal resetGame={resetGame} winner={winner}/>
     </main>
   );
 }
 
 export default App;
-
 
 //min 27
